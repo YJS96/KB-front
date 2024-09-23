6bfd6cdced6e55aa2a10f3849667113a69f290c0
YJS96 <wnt9864@hotmail.com>
2024-09-23 17:49:06 +0900

    carousel

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,9 +4,14 @@ import Medicine from '@/pages/MedicinePage.vue';
 import Prescription from '@/pages/PrescriptionPage.vue';
 import Mypage from '@/pages/ProfilePage.vue';
 import Login from '@/pages/Login/LoginPageKakao.vue';
-import LoginMember from '@/pages/Login/LoginPageInfoMember.vue';
+import LoginPageInfoMember from '@/pages/Login/LoginPageInfoMember.vue';
 import MedicineDetail from '@/pages/MedicineDetail.vue';
 import PrescriptionDetail from '@/pages/PrescriptionDetail.vue';
+import LoginPageInfoUser from '@/pages/Login/LoginPageInfoUser.vue';
+import LoginPageInfoUser2 from '@/pages/Login/LoginPageInfoUser2.vue';
+import LoginPageSuccess from '@/pages/Login/LoginPageSuccess.vue';
+import LoginPageDoctor from '@/pages/Login/LoginPageDoctor.vue';
+import LoginPageDoctor2 from '@/pages/Login/LoginPageDoctor2.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -37,9 +42,34 @@ const router = createRouter({
       component: Login
     },
     {
-      path: '/loginMember',
-      name: 'loginMember',
-      component: LoginMember
+      path: '/login/member',
+      name: 'loginPageInfoMember',
+      component: LoginPageInfoMember
+    },
+    {
+      path: '/login/bank_type',
+      name: '/loginPageInfoUser',
+      component: LoginPageInfoUser
+    },
+    {
+      path: '/login/bank_id',
+      name: '/loginPageInfoUser2',
+      component: LoginPageInfoUser2
+    },
+    {
+      path: '/login/doctor',
+      name: '/loginPageDoctor',
+      component: LoginPageDoctor
+    },
+    {
+      path: '/login/doctor/license',
+      name: '/loginPageDoctor2',
+      component: LoginPageDoctor2
+    },
+    {
+      path: '/success',
+      name: '/loginPageSuccess',
+      component: LoginPageSuccess
     },
     {
       path: '/medicine/:id',
